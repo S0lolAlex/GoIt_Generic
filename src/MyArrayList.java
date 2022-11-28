c8cdc62723923fae1b4a3314c9b067e8409f407a
S0loAlex <lesha1984231@gmail.com>
2022-11-29 00:29:22 +0200

    MyArraList 1.3

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -2,70 +2,79 @@ import java.io.IOException;
 
 public class MyArrayList<T> {
     private final int START_LENGTH = 10;
-    private Object[] array=  new Object[START_LENGTH];
-    private int arraySize;
+    private Object[] array;
+    private int tempSize;
 
 
     public MyArrayList(Object[] array){
-        arraySize = this.array.length;
-        if(array.length < arraySize){
+        initArray();
+        tempSize = this.array.length;
+        if(array.length < tempSize){
         System.arraycopy(array,0,this.array,0,array.length);
         }else{
-            while(array.length > arraySize){
-            this.array = new Object[arraySize * 2];
+            while(array.length > tempSize){
+            this.array = new Object[this.array.length * 2];
+            tempSize = this.array.length;
             }
             System.arraycopy(array,0,this.array,0,array.length);
         }
     }
-    public MyArrayList(){    }
+    public MyArrayList(){
+        initArray();
+    }
+
+    private void initArray(){
+        array = new Object[START_LENGTH];
+    }
 
     public boolean add(T value){
-        arraySize = array.length;
-       int  lastValueIndex;
-        if(array[arraySize - 1] != null){
-             lastValueIndex = arraySize;
-            Object[] result =  new Object[lastValueIndex + 10];
-            System.arraycopy(array,0,result,0,lastValueIndex);
+        tempSize = array.length - 1;
+        if(tempSize == getLastIndex()){
+            Object[] result =  new Object[getLastIndex() + 10];
+            System.arraycopy(array,0,result,0,getLastIndex());
             array = result;
-            array[lastValueIndex] = value;
+            array[getLastIndex()] = value;
         }else{
-            lastValueIndex = 0;
-            while(array[lastValueIndex]!= null){
-                lastValueIndex++;
-            }
-            array[lastValueIndex] = value;
+
+            array[getLastIndex()] = value;
         }
         return true;
     }
 
     public Object remove(int index){
-        arraySize = array.length;
+        tempSize = array.length;
         if(index > size() - 1) throw new IndexOutOfBoundsException("Превышен индекс");
-        Object[] result =  new Object[arraySize - 1];
+        Object[] result =  new Object[tempSize - 1];
         Object valueRemove = array[index];
         if(index == 0){
-            System.arraycopy(array,0,result,1,arraySize - 1);
-        }else if(index == arraySize -1){
+            System.arraycopy(array,0,result,1,tempSize - 1);
+        }else if(index == tempSize -1){
             System.arraycopy(array, 0, result, 0, index - 1);
         }else{
             System.arraycopy(array, 0, result, 0, index);
-            System.arraycopy(array, index + 1, result, index, arraySize - (index + 1) );
+            System.arraycopy(array, index + 1, result, index, tempSize - (index + 1) );
         }
         array = result;
        return valueRemove;
     }
 
     public void clear(){
-        array =  new Object[10];
+        array =  new Object[START_LENGTH];
+    }
+
+    private int getLastIndex(){
+        int lastIndex = 0;
+        if(array[array.length - 1] != null) return array.length;
+        while(array[lastIndex]!=null){
+            lastIndex++;
+        }
+        return lastIndex;
     }
 
     public int size(){
-        int lastValueIndex = 0;
-        if(array[array.length - 1] != null) return array.length;
-        while(array[lastValueIndex]!=null){
-            lastValueIndex++;
-        }
-        return lastValueIndex;
+        if(getLastIndex() == 0) throw new IndexOutOfBoundsException("массив пуст");
+
+        return getLastIndex();
     }
 
     public Object get(int index){
